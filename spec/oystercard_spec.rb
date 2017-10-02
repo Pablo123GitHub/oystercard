80d646bfe6a45fb4ff8ef76faf2ec31bc754d504
Pablo <pabloandresvdl@gmail.com>
2017-10-02 16:42:20 +0100

    add Oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,9 @@
+require "oystercard"
+
 describe Oystercard do
+  let(:oystercard) {described_class}
+  describe "#initialize"
+  it "creates a new Oystercard object with a 'balance' instance variable = 0" do
+    expect(subject.balance).to eq 0
+  end
 end
