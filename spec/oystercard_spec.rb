9f75577912345486caa77b7a67170423cc3402b4
Pablo <pabloandresvdl@gmail.com>
2017-10-02 21:20:09 +0100

    add top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,8 @@ describe Oystercard do
   it "creates a new Oystercard object with a 'balance' instance variable = 0" do
     expect(subject.balance).to eq 0
   end
+  describe '#top_up'
+  it 'tops up an oystercard by a specific amount will add amount to balance' do
+    expect { subject.top_up(5) }.to change { subject.balance }.by(5)
+  end
 end
